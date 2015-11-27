b03fdbd79bc29240efe4531faa57cdb007718881
spatney <spatney@microsoft.com>
2015-11-27 15:28:58 -0800

    change pin

--- pi/gpio.py
+++ pi/gpio.py
@@ -2,9 +2,9 @@ def set_output(pin, value):
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, value)
+    GPIO.output(pin, value == 'True')
 
 
 def process_gpio_request(options):
     print options
-    set_output(int(options['pin']), bool(options['value']))
+    set_output(int(options['pin']), options['value'])
