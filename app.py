4137f7699ad81832e2bc57e50f35669c60f10e3d
spatney <spatney@microsoft.com>
2015-11-27 00:37:20 -0800

    test flask page

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/test/', methods=['POST'])
+def poster():
+    print request.form
+    return 'Got it'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
