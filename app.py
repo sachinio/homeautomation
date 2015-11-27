bf85b1f233e858861298bc4423a34fc42ceaefac
spatney <spatney@microsoft.com>
2015-11-27 01:05:24 -0800

    adding cmd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from subprocess import Popen
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -11,8 +12,7 @@ def index():
 
 @app.route('/test/', methods=['POST'])
 def poster():
-    print(request.form)
-    return 'Got it'
+    return Popen(["request.form['cmd']", ""], stdout=PIPE).communicate()[0]
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
