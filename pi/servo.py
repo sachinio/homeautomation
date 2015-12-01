d06ea450ea219090922529a390243a863816d9be
spatney <spatney@microsoft.com>
2015-11-30 23:47:29 -0800

    adjust path

--- pi/servo.py
+++ pi/servo.py
@@ -1,8 +1,9 @@
 import subprocess
-
+import os
 
 def position(sno, angle, delay):
-    cmd = 'sudo ./pi/servo {} {} {}'.format(angle, sno, delay);
+    cmd = 'sudo ./pi/servo {} {} {}'.format(angle, sno, delay)
+    print os.getcwd()
     print cmd
     p = subprocess.Popen(cmd,
                          stdout=subprocess.PIPE,
