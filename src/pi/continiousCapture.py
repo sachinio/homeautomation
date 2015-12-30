b74f881e6e0fb6e1b1078ec1c7b20e3b86c69073
spatney <spatney@microsoft.com>
2015-12-30 14:09:29 -0800

    updating resolution

--- src/pi/continiousCapture.py
+++ src/pi/continiousCapture.py
@@ -2,7 +2,7 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
+    camera.resolution = (512, 288)
     camera.rotation = 90
     camera.start_preview()
     # Camera warm-up time
