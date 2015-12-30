6eaa8ba3e9c8d69e339e2f3232c6560f76b52e02
spatney <spatney@microsoft.com>
2015-12-30 13:55:13 -0800

    updating rotation

--- src/pi/continiousCapture.py
+++ src/pi/continiousCapture.py
@@ -2,10 +2,11 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
+    camera.resolution = (320, 240)
+    camera.rotation = 90
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
     while True:
-        camera.capture('../../static/ram/picam.jpg', resize=(320, 240))
+        camera.capture('../../static/ram/picam.jpg')
         time.sleep(1)
