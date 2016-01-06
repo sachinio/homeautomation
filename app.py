9fcb42e0301dc7bbfaefcda869685656b7a9ec83
spatney <spatney@microsoft.com>
2016-01-05 20:25:58 -0800

    correcting args

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def camera_internal():
 def camera():
     print(request)
     print(request.args)
-    ip = translate_ip(request.args.get('ip'))
+    ip = translate_ip(request.args['ip'])
     print('ip id is ' + ip)
     return get_camera(ip)
 
